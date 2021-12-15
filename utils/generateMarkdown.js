f76bd220e50980893bd17334307ef31e5b1e077e
Michelle <mchristine.stone91@gmail.com>
2021-12-14 21:55:57 -0500

    updated html with section headers to organize data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,9 +7,18 @@ function generateMarkdown(data) {
 ## Description
 ${data.description}
 
+##Installation
+
+##Usage
 
 ## License
 [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses${data.license})
+
+##Contributing
+
+##Tests
+
+##Questions
 
 `;
 }
