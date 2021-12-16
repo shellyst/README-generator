2d1633c0e8a8c238094cc953bd35244af63a1eba
Michelle <mchristine.stone91@gmail.com>
2021-12-15 22:03:16 -0500

    changed from contributing to contributors as section header

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,7 @@ ${data.usage}
 ## License
 [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses${data.license})
 
-## Contributing
+## Contributors
 ${data.contributors}
 
 ## Tests
